ee6014b5290c577e2248f7c77ad5e51b87ffcb23
Di-Go <alunodiego360@gmail.com>
2020-10-30 23:00:34 -0300

    Grafo - Implementação de grafo

---
+++ src/estrutura/dados/Grafo.java
@@ -0,0 +1,26 @@
+package estrutura.dados;
+
+import java.util.LinkedList;
+
+public class Grafo {
+	
+	protected LinkedList<Vertice> cidades;
+	
+	public Grafo() {
+		this.cidades = new LinkedList<Vertice>();
+	}
+	
+	public void adicioneRota(Vertice origem, Vertice destino, int custo) {
+		origem.Arestas.add(new Aresta(destino, custo));
+		destino.Arestas.add(new Aresta(origem, custo));
+	}
+	
+	public void imprimaRotas() {
+		for (Vertice vertice : cidades) {
+			System.out.println("Nó pai "+vertice.Nome);
+			for (Aresta aresta : vertice.Arestas) {
+				System.out.println("    Nó filho: "+aresta.destino.Nome);
+			}
+		}
+	}
+}
