e623550ee95bc709add043e0e7ff64303086c1e5
gustavo.fernandes <gustavo.fernandes@goias.gov.br>
2020-10-30 23:07:03 -0300

    Atualiza código

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import grafo.Aresta;
 import grafo.Grafo;
 import grafo.Vertice;
+import problema.rotas.GrafoCidades;
 
 public class Main {
 
@@ -21,14 +22,53 @@ public class Main {
 		// arvore.Inserir(6, "no 6");
 		// arvore.Inserir(450, "no 450");
 		
-        Problema();
-        // grafo.imprimeArvore();
-        // System.out.println(grafo.getVertice("Arad").getIncidentes());
-        // System.out.println(grafo.getVertice("Arad").getFilhos());
-        // System.out.println(grafo.getVertice("Arad").getVizinhos());
-        // System.out.println(grafo.getArestas());
-        // System.out.println(grafo.buscaEmProfundidade("Arad", "Bucareste"));
+		GrafoCidades rotas = new GrafoCidades();
+
+        rotas.adicioneRota(rotas.Arad, rotas.Zerind, 75);
+        rotas.adicioneRota(rotas.Arad, rotas.Sibiu, 140);
+        rotas.adicioneRota(rotas.Arad, rotas.Timisoara, 118);
+
+        rotas.adicioneRota(rotas.Zerind, rotas.Oradea, 71);
+
+        rotas.adicioneRota(rotas.Oradea, rotas.Sibiu, 151);
+
+        rotas.adicioneRota(rotas.Timisoara, rotas.Lugoj, 111);
+
+        rotas.adicioneRota(rotas.Lugoj, rotas.Mehadia, 70);
+
+        rotas.adicioneRota(rotas.Mehadia, rotas.Drobeta, 70);
+
+        rotas.adicioneRota(rotas.Drobeta, rotas.Craiova, 120);
+
+        rotas.adicioneRota(rotas.Sibiu, rotas.Fagaras, 99);
+        rotas.adicioneRota(rotas.Sibiu, rotas.Rimnicu, 80);
+
+        rotas.adicioneRota(rotas.Rimnicu, rotas.Pitesti, 97);
+        rotas.adicioneRota(rotas.Rimnicu, rotas.Craiova, 146);
+
+        rotas.adicioneRota(rotas.Craiova, rotas.Pitesti, 138);
+
+        rotas.adicioneRota(rotas.Fagaras, rotas.Bucareste, 211);
+
+        rotas.adicioneRota(rotas.Pitesti, rotas.Bucareste, 101);
+
+        rotas.adicioneRota(rotas.Bucareste, rotas.Giurgiu, 90);
+        rotas.adicioneRota(rotas.Bucareste, rotas.Urziceni, 85);
+
+        rotas.adicioneRota(rotas.Urziceni, rotas.Hirsova, 98);
+        rotas.adicioneRota(rotas.Urziceni, rotas.Vaslui, 142);
+
+        rotas.adicioneRota(rotas.Vaslui, rotas.Iasi, 92);
+
+        rotas.adicioneRota(rotas.Iasi, rotas.Neamt, 87);
+
+        rotas.adicioneRota(rotas.Hirsova, rotas.Eforie, 86);
+
+        rotas.imprimaRotas();
+		
+        
 	}
+	
 	// public static void grafos() {
 	// 	Vertice arad = new Vertice("1");
     //     Vertice no2 = new Vertice("2",no1);
